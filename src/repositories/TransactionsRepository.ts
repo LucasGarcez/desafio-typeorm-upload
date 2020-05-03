8bd66cd036b0b5a4f01fd20437d3538cba3a7d14
lucasgarcez <lucasgarcezdev@gmail.com>
2020-05-03 18:42:46 -0300

    transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -11,7 +11,30 @@ interface Balance {
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    let balance: Balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    balance = transactions.reduce((previousBalance, currentTransaction) => {
+      const accumulatorBalance = previousBalance;
+      if (currentTransaction.type === 'income') {
+        accumulatorBalance.income += Number(currentTransaction.value);
+      }
+
+      if (currentTransaction.type === 'outcome') {
+        accumulatorBalance.outcome += Number(currentTransaction.value);
+      }
+
+      return accumulatorBalance;
+    }, balance);
+
+    return {
+      ...balance,
+      total: balance.income - balance.outcome,
+    };
   }
 }
 
--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,21 +1,49 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import { getCustomRepository } from 'typeorm';
+import { transactionType } from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({
+    transactions,
+    balance,
+  });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category } = request.body;
+
+  const validatedValue = convertToNumber(value);
+  const validatedType = convertToTransactionType(type);
+
+  const createTransactionService = new CreateTransactionService();
+  const transaction = await createTransactionService.execute({
+    title,
+    value: validatedValue,
+    type: validatedType,
+    categoryTitle: category,
+  });
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
+
+  const deleteTransaction = new DeleteTransactionService();
+  await deleteTransaction.execute(id);
+
+  return response.status(204).send();
   // TODO
 });
 
@@ -24,3 +52,18 @@ transactionsRouter.post('/import', async (request, response) => {
 });
 
 export default transactionsRouter;
+
+function convertToNumber(value: string): number {
+  const number = parseInt(value, 10);
+  return number;
+}
+
+function convertToTransactionType(value: string): transactionType {
+  if (value === 'outcome') {
+    return 'outcome';
+  }
+  if (value === 'income') {
+    return 'income';
+  }
+  throw new Error('invalid transaction type');
+}
