ab02d740d688ca648b509185bf70de9cfe8c6436
lucasgarcez <lucasgarcezdev@gmail.com>
2020-05-03 18:43:12 -0300

    create transaction service

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,68 @@
 // import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository, Repository } from 'typeorm';
 
-import Transaction from '../models/Transaction';
+import Transaction, { transactionType } from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+import AppError from '../errors/AppError';
 
+interface TransactionRequest {
+  title: string;
+  value: number;
+  type: transactionType;
+  categoryTitle: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    categoryTitle,
+  }: TransactionRequest): Promise<Transaction> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    await this.canPerformanceTransaction(transactionsRepository, value, type);
+
+    const category = await this.getCategory(categoryTitle);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+      category,
+    });
+
+    await transactionsRepository.save(transaction);
+    return transaction;
+  }
+
+  private async getCategory(categoryTitle: string): Promise<Category> {
+    const categoryRepository = getRepository(Category);
+    const categoryExists = await categoryRepository.findOne({
+      where: { title: categoryTitle },
+    });
+
+    if (categoryExists) {
+      return categoryExists;
+    }
+
+    const category = categoryRepository.create({ title: categoryTitle });
+    await categoryRepository.save(category);
+
+    return category;
+  }
+
+  private async canPerformanceTransaction(
+    transactionsRepository: TransactionsRepository,
+    value: number,
+    type: transactionType,
+  ): Promise<void> {
+    if (type === 'outcome') {
+      const balance = await transactionsRepository.getBalance();
+      if (balance.total < value) {
+        throw new AppError('Insufficient funds');
+      }
+    }
   }
 }
 
