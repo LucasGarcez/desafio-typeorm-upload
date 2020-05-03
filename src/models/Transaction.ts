90fcc8c1f2082dd98163b5701e6fbe75a66f42c1
lucasgarcez <lucasgarcezdev@gmail.com>
2020-05-03 17:54:21 -0300

    create models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,41 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+export type transactionType = 'income' | 'outcome';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
-  type: 'income' | 'outcome';
+  @Column()
+  type: transactionType;
 
+  @Column('decimal')
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Transaction)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
